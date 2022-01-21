90dd26933f4a7ff0de62b5cb7e69240464c2c596
Constance Colmagro <colmgro.constance@orange.fr>
2022-01-21 12:35:06 +0100

    fin abo

--- RudyAIR/src/modelClient/Abonnement.java
+++ RudyAIR/src/modelClient/Abonnement.java
@@ -2,5 +2,16 @@ package modelClient;
 
 public enum Abonnement {
 
-	sansAbonnement(0),Standard(105),Premium(210.90)
+	sansAbonnement(0),Standard(105),Premium(210.90);
+	
+	private double prix;
+	
+	private Abonnement(double prix)
+	{
+		this.prix=prix;
+	}
+	public double getPrix()
+	{
+		return prix;
+	}
 }
