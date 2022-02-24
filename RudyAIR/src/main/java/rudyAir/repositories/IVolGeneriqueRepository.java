5f39745c5f26bd5e21bd1589550f64d73f9823d6
constance <colmagro.constance@orange.fr>
2022-02-24 20:18:45 +0100

    client/compte repo service

--- RudyAIR/src/main/java/rudyAir/repositories/IVolGeneriqueRepository.java
+++ RudyAIR/src/main/java/rudyAir/repositories/IVolGeneriqueRepository.java
@@ -1,10 +1,19 @@
 package rudyAir.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import rudyAir.model.vol.VolGenerique;
 
 
 public interface IVolGeneriqueRepository extends JpaRepository<VolGenerique, Long> {
 
+	@Query("select vg from VolGenerique vg order by vg.prix desc")
+	List<VolGenerique> findVolByPrixDecroissant();
+	
+	@Query("select vg from VolGenerique vg order by vg.prix")
+	List<VolGenerique> findVolByPrixCroissant();
 }	
