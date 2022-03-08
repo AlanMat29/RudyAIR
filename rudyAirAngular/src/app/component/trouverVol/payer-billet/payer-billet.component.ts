bad2df5d38b4d566167458018d21bb9cd4020def
rudyard <rudyardc@hotmail.fr>
2022-03-08 16:25:45 +0100

    Angular Paayement html

--- rudyAirAngular/src/app/component/trouverVol/payer-billet/payer-billet.component.ts
+++ rudyAirAngular/src/app/component/trouverVol/payer-billet/payer-billet.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-payer-billet',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PayerBilletComponent implements OnInit {
 
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+
 
 }
+
+
