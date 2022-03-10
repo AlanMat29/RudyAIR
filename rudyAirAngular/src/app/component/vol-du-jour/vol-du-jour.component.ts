1951b677a3c14a27d711d4a101640e1f75651d81
AlanM <alan-mateu@hotmail.fr>
2022-03-10 02:37:11 +0100

    gitflow-feature-stash: security

---
+++ rudyAirAngular/src/app/component/vol-du-jour/vol-du-jour.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Vol } from 'src/app/model/vol/vol';
+import { VolService } from 'src/app/services/vol.service';
+
+@Component({
+  selector: 'app-vol-du-jour',
+  templateUrl: './vol-du-jour.component.html',
+  styleUrls: ['./vol-du-jour.component.css'],
+})
+export class VolDuJourComponent implements OnInit {
+  vols: Vol[] = [];
+
+  constructor(private volService: VolService) {}
+
+  ngOnInit(): void {
+    this.listAllVol();
+  }
+
+  listAllVol() {
+    this.volService.getAll().subscribe((result) => {
+      this.vols = result;
+    });
+  }
+
+  deleteVolById(id: number) {
+    this.volService.deleteById(id).subscribe((ok) => {
+      this.listAllVol();
+    });
+  }
+
+  canBeDelete(id: number) {
+    for (let v in this.vols) {
+      if (this.vols[v].volGenerique!.id == id) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  convertVolStatutToString(enumStr: any) {
+    if (<string>enumStr == 'onTime') {
+      return 'On time';
+    } else if (<string>enumStr == 'delayed') {
+      return 'Delay';
+    }
+    return 'Canceled';
+  }
+}
