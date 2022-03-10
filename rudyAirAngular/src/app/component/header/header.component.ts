53707f801bcf5c75db9111b18db76229d2ef065d
harry <harry@outlook.com>
2022-03-10 03:05:12 +0000

    connexion et déconnexion fix sur header clients

--- rudyAirAngular/src/app/component/header/header.component.ts
+++ rudyAirAngular/src/app/component/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl('/acceuil');
   }
 
+  get authenticated() {
+    return this.authService.isAuthenticated();
+  }
+
+  get login() {
+    return localStorage.getItem('connexion');
+  }
 }
